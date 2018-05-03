4da7622c0e7e71101d993e1e3be5a544f4b50eea
maxmcd <max.t.mcdonnell@gmail.com>
2018-05-03 12:49:24 -0700

    Prettier + Bump version

--- BroadcastView.js
+++ BroadcastView.js
@@ -1,32 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { PropTypes } from "prop-types";
-import {StyleSheet, requireNativeComponent, NativeModules, View, DeviceEventEmitter, Platform} from 'react-native';
+import {
+  StyleSheet,
+  requireNativeComponent,
+  NativeModules,
+  View,
+  DeviceEventEmitter,
+  Platform
+} from "react-native";
 const BroadcastManager = NativeModules.BroadcastModule;
 
 const styles = StyleSheet.create({
   base: {
-    overflow: 'hidden',
-  },
+    overflow: "hidden"
+  }
 });
 
 export default class BroadcastView extends Component {
-
   setNativeProps(nativeProps) {
     this._root.setNativeProps(nativeProps);
   }
-  componentWillMount(){
-    if(Platform.OS == 'android'){
-      DeviceEventEmitter.addListener('broadcastTimer', (seconds) => {
-        this.props.onBroadcastVideoEncoded({seconds:seconds})
+  componentWillMount() {
+    if (Platform.OS == "android") {
+      DeviceEventEmitter.addListener("broadcastTimer", seconds => {
+        this.props.onBroadcastVideoEncoded({ seconds: seconds });
       });
     }
   }
-  _assignRoot = (component) => {
+  _assignRoot = component => {
     this._root = component;
   };
 
-  _onBroadcastStart = (event) => {
-    if(Platform.OS == 'android'){
+  _onBroadcastStart = event => {
+    if (Platform.OS == "android") {
       BroadcastManager.startTimer(1.1, 3600);
     }
     if (this.props.onBroadcastStart) {
@@ -34,37 +40,37 @@ export default class BroadcastView extends Component {
     }
   };
 
-  _onBroadcastFail = (event) => {
+  _onBroadcastFail = event => {
     if (this.props.onBroadcastFail) {
       this.props.onBroadcastFail(event.nativeEvent);
     }
   };
 
-  _onBroadcastStatusChange = (event) => {
+  _onBroadcastStatusChange = event => {
     if (this.props.onBroadcastStatusChange) {
       this.props.onBroadcastStatusChange(event.nativeEvent);
     }
   };
 
-  _onBroadcastEventReceive = (event) => {
+  _onBroadcastEventReceive = event => {
     if (this.props.onBroadcastEventReceive) {
       this.props.onBroadcastEventReceive(event.nativeEvent);
     }
   };
 
-  _onBroadcastErrorReceive = (event) => {
+  _onBroadcastErrorReceive = event => {
     if (this.props.onBroadcastErrorReceive) {
       this.props.onBroadcastErrorReceive(event.nativeEvent);
     }
   };
 
-  _onBroadcastVideoEncoded = (event) => {
+  _onBroadcastVideoEncoded = event => {
     if (this.props.onBroadcastVideoEncoded) {
       this.props.onBroadcastVideoEncoded(event.nativeEvent);
     }
   };
 
-  _onBroadcastStop = (event) => {
+  _onBroadcastStop = event => {
     if (this.props.onBroadcastStop) {
       this.props.onBroadcastStop(event.nativeEvent);
     }
@@ -80,16 +86,10 @@ export default class BroadcastView extends Component {
       onBroadcastEventReceive: this._onBroadcastEventReceive,
       onBroadcastErrorReceive: this._onBroadcastErrorReceive,
       onBroadcastVideoEncoded: this._onBroadcastVideoEncoded,
-      onBroadcastStop: this._onBroadcastStop,
-
+      onBroadcastStop: this._onBroadcastStop
     });
 
-    return (
-        <RNBroadcastView
-            ref={this._assignRoot}
-            {...nativeProps}
-        />
-    );
+    return <RNBroadcastView ref={this._assignRoot} {...nativeProps} />;
   }
 }
 
@@ -100,7 +100,7 @@ BroadcastView.propTypes = {
   broadcastName: PropTypes.string.isRequired,
   backgroundMode: PropTypes.bool,
   sizePreset: PropTypes.number,
-  videoOrientation: PropTypes.oneOf(['landscape', 'portrait']),
+  videoOrientation: PropTypes.oneOf(["landscape", "portrait"]),
   port: PropTypes.number,
   username: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
@@ -115,7 +115,10 @@ BroadcastView.propTypes = {
   onBroadcastErrorReceive: PropTypes.func,
   onBroadcastVideoEncoded: PropTypes.func,
   onBroadcastStop: PropTypes.func,
-  ...View.propTypes,
+  ...View.propTypes
 };
 
-const RNBroadcastView = requireNativeComponent('RNBroadcastView', BroadcastView);
+const RNBroadcastView = requireNativeComponent(
+  "RNBroadcastView",
+  BroadcastView
+);
